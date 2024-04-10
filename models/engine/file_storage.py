63c9479e4c939df812407a3a2563674226dc095d
prodjohnper <JonathanPerez9743@gmail.com>
2024-04-09 20:23:21 -0400

    Update - Updated file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+'''
+    FileStorage class that serializes instances to a
+    JSON file and deserializes JSON file to instances
+'''
 import json
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    '''
+        Serializes instances to a JSON file and deserializes JSON file to instances
+    '''
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
+        '''
+            Returns the dictionary __objects
+        '''
         if cls is None:
             return FileStorage.__objects
         else:
@@ -20,12 +27,16 @@ class FileStorage:
             return filtered_objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
+        '''
+            Sets in __objects the obj with key <obj class name>.id
+        '''
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """Saves storage dictionary to file"""
+        '''
+            Serializes __objects to the JSON file (path: __file_path)
+        '''
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
@@ -34,7 +45,9 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        '''
+            Deserializes the JSON file to __objects
+        '''
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -44,10 +57,10 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
@@ -60,8 +73,16 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """Deletes obj from __objects"""
+        '''
+            Delete obj from __objects if it’s inside
+        '''
         if obj is not None:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
             if key in FileStorage.__objects:
                 del FileStorage.__objects[key]
+
+    def close(self):
+        '''
+            Call reload() method for deserializing the JSON file to objects
+        '''
+        self.reload()
